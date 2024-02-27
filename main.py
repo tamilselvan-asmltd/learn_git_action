397f7a8dfd9fa574c3621a1be44736fcccf23553
tamilselvan-asmltd <tamilselvan.s@asmltd.com>
2024-02-27 13:57:03 +0530

    otp-generater

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print(34)
+import random
+
+def generate_otp(length=6):
+    """
+    Generate a random OTP of given length.
+
+    Args:
+        length (int): Length of the OTP. Default is 6.
+
+    Returns:
+        str: The generated OTP.
+    """
+    otp = ''.join(random.choices('0123456789', k=length))
+    return otp
+
+if __name__ == "__main__":
+    otp = generate_otp()
+    print("Generated OTP:", otp)
